8cd390cc60404f06e9e0cbcd7120602ca68ffe16
JEEWOO PARK <jeewooget@gmail.com>
2026-01-30 10:12:56 +0900

    hackle_event_데이터 파악하기

--- sql/hackle_events_jeewoo.sql
+++ sql/hackle_events_jeewoo.sql
@@ -52,3 +52,47 @@ FROM (
 ) t
 GROUP BY session_event_cnt
 ORDER BY session_event_cnt;
+
+
+
+-- 26년 1월 29일 작업 
+-- 1. 전체 데이터 개수 및 기간 확인 : 11,441,319 | 23년 7월 18일부터 8월 10일까지 
+SELECT 
+    COUNT(*) AS total_logs,
+    MIN(event_datetime) AS start_date,
+    MAX(event_datetime) AS end_date
+FROM hackle_events
+;
+
+-- 2. 가장 많이 발생하는 이벤트(event_key) TOP 10
+SELECT 
+    event_key, 
+    COUNT(*) AS event_count
+FROM hackle_events
+GROUP BY event_key
+ORDER BY event_count DESC
+LIMIT 10;
+
+-- 특정 이벤트가 발생했을 때 사용자의 평균 지표(친구 수, 하트 잔액 등) 확인
+-- WHERE 자리에 분석하고 싶은 event_key를 넣자! 
+-- 이건 일단 조금 미루기(26.01.29 오후 1:00 기준)
+SELECT 
+    event_key,
+    COUNT(DISTINCT session_id) AS unique_users, -- 참여 유저 수
+    AVG(friend_count) AS avg_friends,          -- 평균 친구 수
+    AVG(heart_balance) AS avg_hearts,          -- 평균 잔여 하트
+    COUNT(question_id) AS total_questions      -- 발생한 총 질문 수
+FROM hackle_events
+WHERE event_key = 'view_lab_tap'
+GROUP BY event_key;
+
+-- 어떤 이벤트가 있는지 event_key 목록 뽑아보기 
+SELECT 
+    event_key, 
+    COUNT(*) AS total_count, -- 발생 횟수
+    COUNT(DISTINCT session_id) AS user_count, -- 얼마나 많은 유저가 수행했는지
+    ROUND(COUNT(*) * 100.0 / (SELECT COUNT(*) FROM hackle_events), 2) AS percentage -- 전체 중 비중
+FROM hackle_events
+GROUP BY event_key
+ORDER BY total_count DESC;
+
